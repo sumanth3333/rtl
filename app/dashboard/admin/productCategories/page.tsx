ed46b630c063ced0113c3b96ccd10bf98bbe95e4
Sumanth Boddu <bssumanth3333@gmail.com>
2025-02-22 21:41:36 -0500

    ready to deploy

--- app/dashboard/admin/productCategories/page.tsx
+++ app/dashboard/admin/productCategories/page.tsx
@@ -1,5 +1,8 @@
 export default function ProductCategoriesPage() {
     // This form takes category like phone, tablet or something as input and ha
-    <AddNewCategoryForm />
-    <AvailableCategories />
+    // <AddNewCategoryForm />
+    // <AvailableCategories />
+    return (
+        <div>Product Categories Page</div>
+    );
 }
